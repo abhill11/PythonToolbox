d9a3f2320fd157678021796fedefaf7f9bcb0020
abhill11 <70242250+abhill11@users.noreply.github.com>
2021-09-08 21:18:54 -0500

    remove redundant comments

--- PythonProject_Script_Pro.py
+++ PythonProject_Script_Pro.py
@@ -4,7 +4,7 @@
 ### in a new Geodatabase
 # Author: Blake Hill
 
-# Import system module
+
 import arcpy
 import sys
 
@@ -19,43 +19,38 @@ GDBLocation = arcpy.GetParameterAsText(3)
 GDBName = arcpy.GetParameterAsText(4)
 ClipLayer = arcpy.GetParameterAsText(5)
 
-# Set workspace environment
+
 arcpy.env.workspace = UDWorkspace
 
-# Folder to store Intermediate Projected Layers
 arcpy.CreateFolder_management(UDWorkspace, "UnclippedProjectedLayers")
 
-# Create File Geodatabase to store Processed Files
 arcpy.CreateFileGDB_management(GDBLocation, GDBName)
 
 GDB = GDBLocation + "\\" + GDBName
 
-# Check for Error in Clipped file name
+# Error Checks
 if '.shp' in FileEnding:
     arcpy.AddError( 'Files saved in Geodatabase cannot have ".shp" file extension')
 else:
     pass
 
-# Check for Errors in GDB Name 
+
 if '.gdb' in GDB:
     pass
 else:
     arcpy.AddError( 'GDB Name must end in ".gdb"')
     sys.exit()
+
     
-# Check for Errors in using a GEOCS instead of PROJCS
 if 'PROJCS' in CoordinateSystem:
     pass
 else:
     arcpy.AddError( 'Please select a **Projected** Coordinate System and run Tool again')
     sys.exit()
 
-
-# List & print workspace featureclasses
 FCList = arcpy.ListFeatureClasses()
 
-# Project input layers to same Coordinate System and Store Intermediate Files in a separate folder
-## for easy deletion afterwards, then batch clip the intermediate layers and save to new GDB
+
 for FC in FCList:
     Output1 = UDWorkspace + "\\" + "UnclippedProjectedLayers" + "\\" + FC.replace('.shp', '_Project.shp')
     Output2 = GDB + "\\" + FC.replace('.shp', FileEnding)
